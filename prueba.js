293336d7aa022a7e1edd77e607c4ecf5446183a1
jlsicajan <sicajancoy@gmail.com>
2017-08-04 04:21:18 -0600

    hello world typescript

---
+++ prueba.js
@@ -0,0 +1,12 @@
+//aqui ponemo nuestro codigo typescript o js que al final es lo mismo
+function helloWorld(nombre) {
+    return "Hello World I'm " + nombre;
+}
+var nombre = "Jose Sicajan";
+var resultado = helloWorld(nombre);
+//la siguiente linea no funcionara porque no se ha cargado el DOM, a menos de que se carge el script en el html al final
+//document.getElementById('container').innerHTML = helloWorld(nombre);
+//si se carga el fichero js en el head
+var etiqueta = document.getElementById('container');
+etiqueta.innerHTML = resultado;
+alert('hello world');
